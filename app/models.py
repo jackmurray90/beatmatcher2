90f8252e2a76cc7ed6ff2290873900eb6c4f05c4
Jack Murray <jack@murray.software>
2023-10-07 13:45:39 +1100

    add booking model

--- app/models.py
+++ app/models.py
@@ -4,7 +4,7 @@ import re
 
 
 def is_valid_username(username):
-    return re.fullmatch("[a-zA-Z0-9_@+.-]*", username) and len(username) > 0 and len(username) <= 150
+    return re.fullmatch(f"[a-zA-Z0-9_@+.-]*", username) and len(username) > 0 and len(username) <= 150
 
 
 class SignUp(models.Model):
@@ -37,18 +37,18 @@ class DJ(models.Model):
     picture = models.BooleanField()
     booking_url = models.CharField(max_length=200, null=True)
     soundcloud_url = models.CharField(max_length=200, null=True)
-    rate = models.IntegerField()
+    rate = models.IntegerField(null=True)
 
 
 class BankDetails(models.Model):
     REGIONS = [
-        ("us", "United States"),
-        ("australia", "Australia"),
-        ("europe", "Europe"),
+        (f"us", "United States"),
+        (f"australia", "Australia"),
+        (f"europe", "Europe"),
     ]
     ACCOUNT_TYPES = [
-        ("checking", "Checking"),
-        ("savings", "Savings"),
+        (f"checking", "Checking"),
+        (f"savings", "Savings"),
     ]
     user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
     region = models.CharField(max_length=100, choices=REGIONS)
@@ -61,3 +61,46 @@ class BankDetails(models.Model):
     account_type = models.CharField(max_length=20, choices=ACCOUNT_TYPES, null=True)
     # Europe, US, and Australia:
     account_number = models.CharField(max_length=100, null=True)
+
+
+class Booking(models.Model):
+    REQUESTED = f"requested"
+    ACCEPTED = f"accepted"
+    DECLINED = f"declined"
+    QUOTE = f"quote"
+    PAID = f"paid"
+    COMPLETED = f"completed"
+    CANCELLED = f"cancelled"
+    REFUNDED = f"refunded"
+    STAGES = [
+        (REQUESTED, "Requested"),
+        (ACCEPTED, "Accepted"),
+        (DECLINED, "Declined"),
+        (QUOTE, "Quote given"),
+        (PAID, "Paid"),
+        (COMPLETED, "Completed"),
+        (CANCELLED, "Cancelled"),
+        (REFUNDED, "Refunded"),
+    ]
+    stage = models.CharField(max_length=200, choices=STAGES)
+    dj = models.ForeignKey(DJ, on_delete=models.CASCADE)
+    code = models.CharField(max_length=23)
+    # Contact details
+    contact_name = models.CharField(max_length=200)
+    phone_number = models.CharField(max_length=50)
+    email = models.EmailField()
+    # Address
+    address_line_1 = models.CharField(max_length=200)
+    address_line_2 = models.CharField(max_length=200)
+    city = models.CharField(max_length=200)
+    state = models.CharField(max_length=200)
+    post_code = models.CharField(max_length=20)
+    country = models.CharField(max_length=200)
+    # Booking
+    set_time = models.DateTimeField()
+    hours = models.IntegerField()
+    equipment_information = models.TextField()
+    other_information = models.TextField()
+    extra_budget = models.IntegerField(null=True)
+    rate = models.IntegerField(null=True)
+    quote = models.IntegerField(null=True)
