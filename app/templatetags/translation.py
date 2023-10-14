a4f3bd93b3f4c21b10db3773819658eb4e11e3a4
Jack Murray <jack@murray.software>
2023-10-14 03:25:08 +0000

    Revert "temporary workaround with translation bug session language missing"
    This reverts commit 8a6ebedde77e5bbaf98e706e71c0d99c537b6a7b.

--- app/templatetags/translation.py
+++ app/templatetags/translation.py
@@ -12,7 +12,7 @@ def with_arg(template, arg):
 def tr(context, string, *args):
     language = context.get("language")
     if not language:
-        language = context["request"].session.get("language", "en")
+        language = context["request"].session["language"]
     if args:
         return app_tr(string, language) % args
     else:
